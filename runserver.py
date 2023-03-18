541cc4f512d99f353350784efe18dba73952b9c6
FrankOnyemaOrji <f.orji@alustudent.com>
2023-03-18 12:44:36 +0200

    Changes done

--- runserver.py
+++ runserver.py
@@ -1,7 +1,10 @@
 from API import create_app
 from API.config.config import config_dict
 
+import os
+
 app = create_app(config=config_dict['prod'])
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=True)
